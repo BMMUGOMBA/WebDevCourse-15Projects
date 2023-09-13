19cc6332d2727719cd16898809fc5e7abda318e1
ProfBatsi <bmmugomba@gmail.com>
2023-09-13 20:53:50 +0200

    start/reset fxn

--- 2-MathsGame_JS/javascript.js
+++ 2-MathsGame_JS/javascript.js
@@ -12,5 +12,34 @@ document.getElementById("startreset").onclick=function(){
 
     if(playing == true){
         location.reload();//reload page
+    } else {
+        //if we are not playing
+        //change mode to  playing
+        playing = true;
+
+        //set score to 0
+        score = 0;
+        document.getElementById("scorevalue").innerHTML = score;
+
+        //show countdown box
+
+        show("timeremaining");
+        timeremaining = 60;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        
+        //hide game over box
+        
+        hide("gameOver");
+        
+        //change button to reset
+        document.getElementById("startreset").innerHTML = "Reset Game";
+        
+        //start countdown
+        
+        startCountdown();
+        
+        //generate a new Q&A
+        
+        generateQA();
     }
 }
